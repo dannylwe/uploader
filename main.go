c6dc47b2c1cd4e8c31fc68015af3ecd2114d73a5
Daniel Lwetabe <acehanks@gmail.com>
2020-09-20 16:14:33 +0300

    not working

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"mime"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"text/template"
@@ -54,6 +57,11 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// if err := confirmFileType(file, w); err != nil {
+	// 	fmt.Printf("%v", err)
+	// 	return
+	// }
+
 	if err := validateFileSize(handler.Size, maxUploadSize, w); err != nil {
 		fmt.Printf("%v\n", err)
 		return;
@@ -92,9 +100,32 @@ func renderError(w http.ResponseWriter, message string, statusCode int) {
 }
 
 func validateFileSize(fileSize, maxSize int64, w http.ResponseWriter) error {
-	if(fileSize > maxSize) {
+	if(fileSize > maxSize/100000) {
 		renderError(w, "File Too Large", http.StatusRequestEntityTooLarge)
+		fmt.Println(fileSize, maxSize/100000)
 		return errors.New("File too big")
+	}
+	return nil
+}
+
+func confirmFileType(file multipart.File, w http.ResponseWriter) error {
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		renderError(w, "INVALID_FILE\n", http.StatusBadRequest)
+		return err
+	}
+
+	// check file type
+	detectedFileType := http.DetectContentType(fileBytes)
+	if(detectedFileType != "text/csv" && detectedFileType != "application/vnd.ms-excel") {
+		renderError(w, "INVALID_FILE_TYPE\n", http.StatusBadRequest)
+		return err
+	}
+
+	_, err = mime.ExtensionsByType(detectedFileType)
+	if err != nil {
+		renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
+		return err
 	}
 	return nil
 }

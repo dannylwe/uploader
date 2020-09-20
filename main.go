f7060f4cd77c16ec5828f4c31572c571606d01f1
Daniel Lwetabe <acehanks@gmail.com>
2020-09-20 12:11:39 +0300

    Basic server: - TODO: implement uploadFile functionality

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/upload", uploadHandler)
+
+	http.ListenAndServe(":9000", nil)
+}
+
+// compile template
+var templates = template.Must(template.ParseFiles("public/upload.html"))
+
+// Display template
+func display(w http.ResponseWriter, page string, data interface{}) {
+	templates.ExecuteTemplate(w, page + ".html", data)
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		display(w, "upload", nil)
+	case "POST":
+		uploadFile(w, r)
+	}
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Uploading File")
 }

35a7a5942bfbc50d064fe7bc21d057975d65dd35
Daniel Lwetabe <acehanks@gmail.com>
2020-09-21 12:18:08 +0300

    get all endpoint complete

--- model/sales.go
+++ model/sales.go
@@ -5,17 +5,17 @@ import "github.com/jinzhu/gorm"
 // Sales is struct to database
 type Sales struct {
 	gorm.Model
-	Region       string
-	Country      string
-	ItemType     string
-	SalesChannel string
-	OrderPrice   string
-	OrderDate    string
-	OrderID      string
-	ShipDate     string
-	UnitsSold    string
-	UnitPrice    string
-	TotalRevenue string
-	TotalCost    string
-	TotalProfit  string
+	Region       string `json:"region"`
+	Country      string	`json:"country"`
+	ItemType     string `json:"ItemType"`
+	SalesChannel string `json:"SalesChannel"`
+	OrderPrice   string `json:"OrderPrice"`
+	OrderDate    string `json:"OrderDate"`
+	OrderID      string `json:"OrderID"`
+	ShipDate     string `json:"ShipDate"`
+	UnitsSold    string `json:"UnitsSold"`
+	UnitPrice    string `json:"UnitPrice"`
+	TotalRevenue string `json:"TotalRevenue"`
+	TotalCost    string `json:"TotalCost"`
+	TotalProfit  string `json:"TotalProfit"`
 }

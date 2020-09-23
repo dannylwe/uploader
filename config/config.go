40c845837c1334a5d485c03cc96323d14b9e1dbc
Daniel Lwetabe <acehanks@gmail.com>
2020-09-23 11:27:54 +0300

    working directory error

--- config/config.go
+++ config/config.go
@@ -1,10 +1,7 @@
 package config
 
 import (
-	"os"
-	"runtime"
-	"strings"
-
+	
 	"github.com/danny/services/handlers"
 	"github.com/danny/services/model"
 )
@@ -14,14 +11,4 @@ func Run() {
 	model.ConnectDatabase()
 	model.SQLConn()
 	handlers.SetupRoutes()
-}
-
-func GetWD() string{
-	dir, _ := os.Getwd()
-	var ss [] string
-  	if runtime.GOOS == "windows" {
-		ss = strings.Split(dir, "\\")
-		 
-	}
-	return ss[len(ss)-1]
 }
--- handlers/render.go
+++ handlers/render.go
@@ -4,13 +4,37 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
-var wd, _ = os.Getwd()
 
+// var wd = getWD()
+var dir, _ = os.Getwd()
+var newDir = strings.Replace(dir, "\\", "/", -1)
+// var newer = strings.Split(newDir, "/")
+var pathing, _ = filepath.Split(newDir)
+var absPath, _ = filepath.Abs("./public/upload.html")
 // compile template
-var templates = template.Must(template.ParseFiles("C:/Users/pc/Desktop/projects/uploader/"+ "/public/upload.html"))
+// var templates = template.Must(template.ParseFiles(newDir + "/public/upload.html"))
+// var templates = template.Must(template.ParseFiles(pathing + "public/upload.html"))
+
+var templates = template.Must(template.ParseFiles(filepath.Join(newDir, "./public/upload.html")))
+
 
 // Display template
 func Display(w http.ResponseWriter, page string, data interface{}) {
 	templates.ExecuteTemplate(w, page+".html", data)
+}
+
+func getWD() string{
+	dir, _ := os.Getwd()
+	var ss [] string
+  	if runtime.GOOS == "windows" {
+		var st = strings.Replace(dir, "\\", "/", -1)
+		ss = strings.Split(st, "/")
+	} else {
+		ss = strings.Split(dir, "/")
+	}
+	return ss[len(ss)-1]
 }
